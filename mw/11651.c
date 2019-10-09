f0072be5cb34e6ab600aa4d5195e8dbf9744271c
Kim Minwoo <43179854+davidkmw0810@users.noreply.github.com>
2019-10-10 01:21:41 +0900

    runtime error with 11651

--- mw/11651.c
+++ mw/11651.c
@@ -4,14 +4,13 @@ int list[100000][2];
 
 void sort(int p, int l, int r, int xy)
 {
-    printf("l, r = %d %d\n", l, r);
 
     int buffer[1][2];
     int i = l;
     int j = r;
     
     while(i < j)
-    {
+    {   
         while((int)list[p][xy] >= (int)list[i][xy] && i < r){i++;}
         while((int)list[p][xy] < (int)list[j][xy] && j > l){j--;}
 
@@ -20,7 +19,7 @@ void sort(int p, int l, int r, int xy)
             buffer[0][xy] = list[i][xy];
             list[i][xy] = list[j][xy];
             list[j][xy] = buffer[0][xy];
-            printf("i, j = %d %d\nxy = %d %d\n", i, j, xy, xy^1);
+
             buffer[0][xy^1] = list[i][xy^1];
             list[i][xy^1] = list[j][xy^1];
             list[j][xy^1] = buffer[0][xy^1];
@@ -35,15 +34,11 @@ void sort(int p, int l, int r, int xy)
     buffer[0][xy^1] = list[p][xy^1];
     list[p][xy^1] = list[j][xy^1];
     list[j][xy^1] = buffer[0][xy^1];
-    
-    for(int ii = 0; ii < 5; ii++)
-    {
-        printf("a = %d, %d\n", list[ii][0], list[ii][1]);
-    }
-    printf("\n");
-    
-    sort((int)(l + j - 1)/2, l, j - 1, xy);
-    sort((int)(j + 1 + r)/2, j + 1, r, xy);
+       
+    if(p > l + 1)
+        sort((int)(l + j - 1)/2, l, p - 1, xy);
+    if(p < r)
+        sort((int)(j + 1 + r)/2, p, r, xy);
 
 }
 
@@ -57,20 +52,22 @@ int main()
         scanf("%d %d", &list[i][0], &list[i][1]);
     }
 
-    /*
-    for(i = 0; i < n; i++)
-    {
-        printf("list = %d, %d, %d\n", list[i][0], list[i][1], (int)list[i][0] + (int)list[i][1]);
-    }
-    */
-
     sort((int)(n-1)/2, 0, n - 1, 1);
-    //sort((int)n/2, 0, n - 1, 0);
     
-    printf("a\n");
+    for(i = 0; i < n - 1; i++)
+    {
+        if(list[i][1] == list[i + 1][1])
+        {
+            int k = i + 2;
+            while(list[i][1] == list[i + k][1]){k++;}
+            sort((int)(i + k)/2, i, k, 0);
+            i = k;
+        }
+    }
+
     for(i = 0; i < n; i++)
     {
-        printf("a = %d, %d\n", list[i][0], list[i][1]);
+        printf("%d, %d\n", list[i][0], list[i][1]);
     }
     
     return 0;
